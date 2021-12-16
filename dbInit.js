8ec3ff818efc2f4248d6bf6213d0791e12cbfb70
stella <0604seohee@gmail.com>
2021-12-16 16:33:47 +0900

    username, password updated

--- dbInit.js
+++ dbInit.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("database", "username", "password", {
+const sequelize = new Sequelize("database", "root", process.env.password, {
     host: "localhost",
     dialect: "sqlite",
     logging: false,
